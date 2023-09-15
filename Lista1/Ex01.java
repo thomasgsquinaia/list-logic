0d85b95c731ae4142f034a3ac33335818fc361c2
thomasgsquinaia <thomasquinaia@gmail.com>
2023-09-15 16:06:50 -0300

    [main] main: lista001 Ex01

---
+++ Lista1/Ex01.java
@@ -0,0 +1,19 @@
+/*
+  Desenvolva o algoritmo de um programa onde o usuário irá informar um número
+  inteiro e o programa deve calcular e exibir o número imediatamente antecessor ao
+  número digitado pelo usuário.
+*/
+
+import java.util.Scanner;
+
+class Ex01 {
+  public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    System.out.println("Digite um número: ");
+    
+    int num = read.nextInt();
+    int antecessor= num - 1 ;
+    System.out.printf("Número digitado inicialmente: %d\n", num);
+    System.out.printf("Antecessor: %d", antecessor);
+  } 
+}
