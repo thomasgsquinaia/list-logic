085d9fc5e3d2683d5b67a96ab5335f4ef1bd7978
thomasgsquinaia <thomasquinaia@gmail.com>
2023-09-15 16:32:51 -0300

    [main] main: lista001 Ex03 Ex04

---
+++ Lista1/Ex03.java
@@ -0,0 +1,18 @@
+/*
+  Desenvolva o algoritmo de um programa onde o usuário irá informar um número
+  inteiro e o programa deve calcular e exibir quadrado do número informado pelo
+  usuário.
+*/
+
+import java.util.Scanner;
+
+class Ex03 {
+  public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    System.out.print("Digite um número: ");
+
+    float num = read.nextFloat();
+    float sum = num * num;
+    System.out.printf("Quadrado do número informado: %.2f", sum);
+  }  
+}
---
+++ Lista1/Ex04.java
@@ -0,0 +1,20 @@
+/*
+  Desenvolva um algoritmo para calcular a área de um triângulo. Pensem nas
+  variáveis que serão necessárias. Ao final, o algoritmo deve informar a área total do
+  triângulo.
+*/
+
+import java.util.Scanner;
+
+class Ex04 {
+  public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    System.out.print("Digite a base do seu triângulo: \n");
+    float base = read.nextFloat();
+    System.out.print("Digite a altura do seu triângulo: \n");
+    float altura = read.nextFloat();
+
+    float area = (base * altura) / 2;
+    System.out.printf("Área total do triângulo: %.2f", area);
+  }
+}
