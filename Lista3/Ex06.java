07004f0b65e73791b004768d5630d1b8a083cbb2
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-10 01:22:08 -0300

    [main] main: lista003 Ex06

--- Lista3/Ex06.java
+++ Lista3/Ex06.java
@@ -11,13 +11,33 @@ import java.util.Scanner;
 public class Ex06 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
+    String timeVencedor = "";
 
-    System.out.println("Digite um nome de time: ");
+    System.out.print("Digite um nome de time: ");
     String timeUm = reader.next();
-    System.out.println("Digite o nome de outro time: ");
-    String timeDois = reader.next();
+    System.out.printf("Digite quantos gols marcados o %s fez na partida: ", timeUm);
+    int golsTimeUm = reader.nextInt();
 
-    
-    
+    System.out.print("Digite um nome de time: ");
+    String timeDois = reader.next();
+    System.out.printf("Digite quantos gols marcados o %s fez na partida: ", timeDois);
+    int golsTimeDois = reader.nextInt();
+
+    if(golsTimeUm > golsTimeDois) {
+      timeVencedor = timeUm;
+      System.out.printf("Time vencedor: %s", timeVencedor);
+    } 
+    else if(golsTimeDois > golsTimeUm) {
+      timeVencedor = timeDois;
+      System.out.printf("Time vencedor: %s", timeVencedor);
+    }    
+    else if(golsTimeDois == golsTimeUm) {
+      System.out.println("EMPATE");
+    }
+    else {
+      System.out.println("Algum dado está incorreto!!");
+    }
+
+    reader.close();
   }
 }
