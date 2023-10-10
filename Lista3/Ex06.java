212a92975c2f9c5c708394f5f9df6a11e9f196a0
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-09 23:12:25 -0300

    [main] main: lista003 Ex06

---
+++ Lista3/Ex06.java
@@ -0,0 +1,23 @@
+package Lista3;
+
+import java.util.Scanner;
+
+/*
+  Ler o nome de dois times e o número de gols marcados na partida (para cada time).
+  Escrever o nome do vencedor. Caso não haja vencedor deverá ser impressa a
+  palavra EMPATE.
+*/
+
+public class Ex06 {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    System.out.println("Digite um nome de time: ");
+    String timeUm = reader.next();
+    System.out.println("Digite o nome de outro time: ");
+    String timeDois = reader.next();
+
+    
+    
+  }
+}
