0b1ef85a456ca34a8ff15120ad9b33bf63747dac
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-11 18:28:28 -0300

    [main] main: lista003 Ex07

--- Lista3/Ex07.java
+++ Lista3/Ex07.java
@@ -1,9 +1,6 @@
 package Lista3;
 
-import java.text.ParseException;
 import java.util.Scanner;
-
-import javax.sound.sampled.SourceDataLine;
 
 /*
   Um posto está vendendo combustíveis com a seguinte tabela de descontos:
@@ -15,30 +12,80 @@ import javax.sound.sampled.SourceDataLine;
   (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser
   pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 4.30 e o preço do
   litro do álcool é R$ 3.90
-  
 */
 
 public class Ex07 {
-  public static void main(String[] args) throws ParseException {
+  public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
-    float combustivel = 0;
+    // float aux = 0;
+    float desconto = 0;
 
-    System.out.printf("Digite o número de litros vendidos: ");
+    System.out.print("Digite o número de litros vendidos: ");
     float litros = reader.nextFloat();
-    System.out.printf("Digite o tipo do combustível A-álcool G-gasolina: ");
+    System.out.print("-------------------------------------------\n");
+    System.out.print("=> A-álcool \n=> G-gasolina \nDigite o tipo do combustível: ");
     String tipoCombustivel = reader.next();
 
-    if(tipoCombustivel == "A") {
-      float valorPagoAlcool = litros * 3.90f;
-      combustivel = valorPagoAlcool;
+    switch (tipoCombustivel) {
+      case "A":
+        if (litros <= 20) {
+          float descontoAlcool = 0.03f;
+          desconto = descontoAlcool;
+        }
+        if (litros > 20) {
+          float descontoAlcool = 0.05f;
+          desconto = descontoAlcool;
+        }
+        float valorPagoAlcool = (litros * 3.90f) - ((litros * 3.9f) * desconto);
+        System.out.printf("Valor pago: R$%.3f", valorPagoAlcool);
+        break;
+      case "G":
+        if (litros <= 20) {
+          float descontoGasolina = 0.04f;
+          desconto = descontoGasolina;
+        }
+        if (litros > 20) {
+          float descontoGasolina = 0.06f;
+          desconto = descontoGasolina;
+        }
+        float valorPagoGasolina = (litros * 4.30f) - ((litros * 4.30f) * desconto);
+        System.out.printf("Valor pago: R$%.3f", valorPagoGasolina);
+        break;
+      default:
+        System.out.println("Algum dado está errado!");
     }
-    if(tipoCombustivel == "G") {
-      float valorPagoGasolina = litros * 4.30f;
-      combustivel =  valorPagoGasolina;
-    }
-    
-    System.out.printf("Valor pago: %.2f", combustivel);
+
+    // System.out.println("fora 1");
+    // if (tipoCombustivel == "A") {
+    //   System.out.println("dentro 1");
+    //   if (litros <= 20) {
+    //     float descontoAlcool = 0.03f;
+    //     desconto = descontoAlcool;
+    //   }
+    //   if (litros > 20) {
+    //     float descontoAlcool = 0.05f;
+    //     desconto = descontoAlcool;
+    //   }
+    //   float valorPagoAlcool = (litros * 3.90f) - ((litros * 3.9f) * desconto);
+    //   aux = valorPagoAlcool;
+    // }
+    // System.out.println("fora 2");
+    // if (tipoCombustivel == "G") {
+    //   if (litros <= 20) {
+    //     float descontoGasolina = 0.04f;
+    //     desconto = descontoGasolina;
+    //   }
+    //   if (litros > 20) {
+    //     float descontoGasolina = 0.06f;
+    //     desconto = descontoGasolina;
+    //   }
+    //   float valorPagoGasolina = (litros * 4.30f) - ((litros * 4.30f) * desconto);
+    //   aux = valorPagoGasolina;
+    // }
+    // System.out.println("fora 3");
+
+    // System.out.printf("%.2f", aux);
 
     reader.close();
-  }  
+  }
 }
