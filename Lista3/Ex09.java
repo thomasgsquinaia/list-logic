92e32d4b9709a03dd2e0f23ac8928b772f9f842d
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-17 00:17:05 -0300

    [main] main: lista003 Ex09

--- Lista3/Ex09.java
+++ Lista3/Ex09.java
@@ -6,9 +6,12 @@ import java.util.Scanner;
   Escreva um algoritmo que leia:
     as notas das duas avaliações normais 
     e a nota da avaliação optativa. 
+  
   Caso o aluno não tenha feito a optativa deve ser fornecido o valor –1. 
+  
   Calcular a média do semestre considerando que a prova optativa substitui a nota
   mais baixa entre as duas primeiras avaliações.
+
   Escrever a média e mensagens que indiquem 
     se o aluno foi aprovado, 
     reprovado ou está em exame, de acordo com as informações abaixo:;
@@ -22,7 +25,8 @@ public class Ex09 {
     Scanner reader = new Scanner(System.in);
     float p1 = 0;
     float p2 = 0;
-    float provaOptativa;
+    float provaOptativa = 0;
+    float mediaNotas = 0;
 
     System.out.println("Digite a nota da primeira avaliação: ");
     p1 = reader.nextFloat();
@@ -30,26 +34,33 @@ public class Ex09 {
     p2 = reader.nextFloat();
     System.out.println("Digite a nota da avaliação optativa: (caso nao tenha nota digite 0)");
     provaOptativa = reader.nextFloat();
-
-    if(provaOptativa == 0) {
-      provaOptativa = -1;
-    }
-    if(p1 > p2) {
-      provaOptativa = p2;
-    }
-    if(p2 > p1) {
-      provaOptativa = p1;
-    }
-
-    float mediaNotas = (p1 + p2 + provaOptativa) / 2;
     
-    if(mediaNotas >= 6.0f) {
+    // considerando que a prova optativa substitui a nota
+    // mais baixa entre as duas primeiras avaliações. 
+    if(p1 < p2) {
+      p1 = provaOptativa;//menor
+    }
+    if(p2 < p1) {
+      p2 = provaOptativa;// menor
+    }
+    // if(provaOptativa == 0) {
+    //   provaOptativa = -1;
+    //   mediaNotas = ((p1 + p2) / 2) - provaOptativa;
+    // }
+
+    mediaNotas = ((p1 + p2) / 2);
+    System.out.printf("p1: %f\n",p1);
+    System.out.printf("p2: %f\n",p2);
+    System.out.printf("provaOp: %f\n",provaOptativa);
+    System.out.println("--------------------------------------------");
+
+    if(mediaNotas >= 6f) {
       System.out.printf("Aprovado\nmedia:%.2f", mediaNotas);
     }
-    if(mediaNotas >= 3.0f && mediaNotas < 6.0f) {
+    if(mediaNotas >= 3f && mediaNotas < 6f) {
       System.out.printf("Exame\nmedia:%.2f", mediaNotas);
     }
-    if(mediaNotas < 3.0f) {
+    if(mediaNotas < 3f) {
       System.out.printf("Reprovado\nmedia:%.2f", mediaNotas);
     }
     
