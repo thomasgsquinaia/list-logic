6868b37d564c6fb5afc076c0de19ba3b03050a9b
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-18 19:05:43 -0300

    [main] main: lista004 Ex01

---
+++ Lista4/Ex01.java
@@ -0,0 +1,11 @@
+package Lista4;
+
+/*
+  Ler um número inteiro n. Escrever a soma de todos os números de 1 até n.
+*/
+
+public class Ex01 {
+  public static void main(String[] args) {
+    
+  }
+}
