686a1fbf9e09d1df458b8a1e8769ab8eb60b449a
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-19 00:33:40 -0300

    [main] main: lista004 Ex01

--- Lista4/Ex01.java
+++ Lista4/Ex01.java
@@ -1,11 +1,26 @@
 package Lista4;
 
+import java.util.Scanner;
+
 /*
   Ler um número inteiro n. Escrever a soma de todos os números de 1 até n.
 */
 
 public class Ex01 {
   public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
     
+    int n = 0;
+    int i = 0;
+    int soma = 0;
+    System.out.println("Digite um número inteiro: ");
+    n = reader.nextInt();
+
+    for(i = 1; i <= n; i++) {
+      soma += n;
+    }
+    System.out.printf("Soma: %d\n", soma);
+
+    reader.close();
   }
 }
