01f031cf5958a887659c1cec78cca8ab4262ba38
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-25 17:23:47 -0300

    [main] main: lista04 Ex09

---
+++ Lista4/Ex09.java
@@ -0,0 +1,39 @@
+package Lista4;
+
+import java.util.Scanner;
+
+/*
+  Escreva um algoritmo para ler uma idade, mas o algoritmo deve aceitar somente
+  idade válida, ou seja, o algoritmo deve “validar” a leitura da idade. Idade válida tem
+  que ser maior que 0 e menor que 150. Escrever a idade lida.
+*/
+
+public class Ex09 {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    int idade = 0;
+
+    System.out.println("Digite a sua idade: ");
+    idade = reader.nextInt();
+    
+    while(idade > 0 && idade < 150) {
+      System.out.println("Digite a sua idade:");
+      idade = reader.nextInt();
+
+      if(idade > 0 && idade < 150) {
+        System.out.println("================================");
+        System.out.printf("Idade válida!!!\nIdade: %d anos\n", idade);
+        System.out.println("================================");
+      } 
+      if(idade < 0 || idade > 150) {
+        System.out.print("Idade menor que 0 ou mair que 150");
+      } 
+     
+      idade++;
+    }
+
+    reader.close();
+
+  }
+}
