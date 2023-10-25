3d30827a3c145429e1909b709fad911cf709a68b
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-25 14:10:24 -0300

    [main] main: lista04 Ex08

--- Lista4/Ex08.java
+++ Lista4/Ex08.java
@@ -14,6 +14,25 @@ public class Ex08 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
 
+    int aux = 0;
+    float qtdProdutos = 0;
+    float valorProd = 0;
+    float sumAuxProd = 0;
+    float media = 0;
+    
+    System.out.print("Digite o número total de mercadorias no estoque deste produto: ");
+    qtdProdutos = reader.nextInt();
+    while(aux < qtdProdutos) {
+      System.out.print("Digite o valor do produto: ");
+      valorProd = reader.nextInt();
+      sumAuxProd+=valorProd;
+      aux++;
+    }
+    media = sumAuxProd/aux;
+
+    System.out.printf("Valor total em estoque: %.2f\n", sumAuxProd);
+    System.out.printf("Média dos valores das mercadorias: %.2f", media);
+
     reader.close();
   }
 }
