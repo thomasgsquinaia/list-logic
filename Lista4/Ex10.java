e247ae0a2c2dea1a40e8aa06415f236a74dfe42a
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-27 23:20:49 -0300

    [main] main: lista04 Ex10

---
+++ Lista4/Ex10.java
@@ -0,0 +1,33 @@
+package Lista4;
+
+import java.util.Scanner;
+
+/*
+  Escreva um algoritmo para ler um salário. O algoritmo deve “validar” a leitura do
+  salário para aceitar somente um “salário válido”. Você deve definir o que é um
+  “salário válido”. Escrever o salário lido.
+*/
+
+public class Ex10 {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    float salario = 10000;
+    float salarioInput = 0;
+
+    System.out.println("Digite um salário: (Salário válido é maior ou igual a 10000)");
+    salarioInput = reader.nextFloat();
+
+    while(salario != salarioInput) {
+      System.out.println("Salário invalido");
+    
+      System.out.println("Digite um salário: ");
+      salarioInput = reader.nextFloat();
+    }
+
+    System.out.printf("Salário válido\n=> %.2f", salarioInput);
+
+
+    reader.close();
+  }
+}
