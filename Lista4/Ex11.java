d745209898bde7ea776bf1be77e6ff4184f5311d
thomasgsquinaia <thomasquinaia@gmail.com>
2023-10-30 14:26:56 -0300

    [main] main: lista04 fix Ex11

--- Lista4/Ex11.java
+++ Lista4/Ex11.java
@@ -12,7 +12,25 @@ import java.util.Scanner;
 public class Ex11 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
+    
+    float num1 = 0;
+    float num2 = 0;
+    float divisao = 0;
 
+    System.out.println("Digite um valor: ");
+    num1 = reader.nextFloat();
+    System.out.println("Digite um segundo valor:");
+    num2 = reader.nextFloat();
+    
+    if(num2 == 0) {
+      while(num2 == 0) {
+        System.out.println("Digite um segundo valor novamente:");
+        num2 = reader.nextFloat();
+      }
+    }
+    divisao = num1/num2;
+    
+    System.out.printf("Resultado: %.2f", divisao);
 
     reader.close();
   }
