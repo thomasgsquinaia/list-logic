2dbe0429dd508a41250d71584060f94fea57192f
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-01 18:18:18 -0300

    [main] main: lista04 init Ex15

---
+++ Lista4/Ex15.java
@@ -0,0 +1,17 @@
+package Lista4;
+
+import java.util.Scanner;
+
+/*
+  Fazer um algoritmo que leia um número inteiro e escreva se ele é ou não um
+  número primo. Um número é primo quando ele é divisível somente por um e por ele
+  mesmo. Você utilizará uma combinação entre os comandos while e if.
+*/
+
+public class Ex15 {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    reader.close();
+  }
+}
