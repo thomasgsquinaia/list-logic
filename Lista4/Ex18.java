51763753bb49e20d59def152a18bc538eed41cfa
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-10 13:58:46 -0300

    [main] main: lista04 Ex18 megazord

--- Lista4/Ex18.java
+++ Lista4/Ex18.java
@@ -16,11 +16,11 @@ public class Ex18 {
     Scanner reader = new Scanner(System.in);
 
     float idadeMedia = 0;
-    float n = 1;
-    float menorIdade = 0;
-    float maiorIdade = 0;
+    int n = 1;
+    int menorIdade = 0;
+    int maiorIdade = 0;
     int sum = 0;
-    float countPessoa = 0;
+    int countPessoa = 0;
     float media = 0;
 
     while (n != 0) {
@@ -33,17 +33,26 @@ public class Ex18 {
       countPessoa++;
       media=sum/countPessoa;
 
-      menorIdade=n;
-   
-      
-    }
-    System.out.println("----------------------------------------");
-    System.out.printf("número de pessoas: %.0f\n", countPessoa);
-    System.out.printf("média: %.2f\n", media);
-    System.out.printf("Menor idade: %.2f", menorIdade);
-    System.out.printf("Maior idade: %.2f\n", maiorIdade);
- 
+      if(maiorIdade < n) {
+        maiorIdade=n;
+      } 
 
+      if(menorIdade==0) {
+        menorIdade=n;
+      }
+
+      if(menorIdade > n) {
+        menorIdade=n;
+      }
+   
+    }
+
+    System.out.println("----------------------------------------");
+    System.out.printf("número de pessoas: %d\n", countPessoa);
+    System.out.printf("média: %.2f\n", media);
+    System.out.printf("Menor idade: %d\n", menorIdade);
+    System.out.printf("Maior idade: %d\n", maiorIdade);
+ 
     reader.close();
   }
 }
