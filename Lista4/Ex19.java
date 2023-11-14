c25f0c88f0e372887d9a64049ca93b93c9bb71ad
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-13 23:55:37 -0300

    [main] main: lista04 Ex19 fibo

--- Lista4/Ex19.java
+++ Lista4/Ex19.java
@@ -9,19 +9,31 @@ import java.util.Scanner;
   A sequência de Fibonacci parte do princípio de que o seu próximo número será a soma dos dois números anteriores. 
   Sendo assim, temos a seguinte progressão: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584… 
   Como você pode perceber, todos os números são a soma dos dois algoritmos anteriores.
-*/
+
+  tenho que pegar o numero 2 
+  */
 
 public class Ex19 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
     
+    int i = 1;
     int n=0;
-    int aux=0;
-
+    int ant=0;
+    int valor1=0;
+    int valor2=1;
+    // O RESULTADO MAIS O ANTERIOR
+    //1+1=2+3=5+3=8...
+    System.out.print("Digite um número: ");
     n=reader.nextInt();
-    for(int i = 1; i < n; i++){
-      System.out.printf("to fora %d",i);
+    for(i = 1; i <= n; i++){
+      // System.out.printf("to dentro %d\n",i);
+      valor1=valor1+i;
+
+      System.out.printf("valor1: %d\n",valor1);
+      System.out.printf("i: %d\n",i);
     }
+
 
     reader.close();
   }
