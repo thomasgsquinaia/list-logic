ad5062a9f84b31b07d9ebfb70f9d313f40edf256
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-17 17:26:53 -0300

    [main] main: lista04 Ex22 init

---
+++ Lista4/Ex22.java
@@ -0,0 +1,19 @@
+package Lista4;
+
+import java.util.Scanner;
+
+/*
+  Dado um país A, com X habitantes e uma taxa de natalidade de 3% ao ano, e um
+  país B com Y habitantes e uma taxa de natalidade de 2% ao ano, escrever um
+  algoritmo em JAva que seja capaz de calcular e no fim imprimir o tempo necessário
+  para que a população do país A ultrapasse a população do país B. Considere que X < Y.
+*/
+
+public class Ex22 {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    reader.close();
+  }
+
+}
