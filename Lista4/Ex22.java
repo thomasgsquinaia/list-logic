3f64615ab207c5bd09fa4601c63122327f49983c
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-18 19:35:41 -0300

    [main] main: lista04 Ex22 finish

--- Lista4/Ex22.java
+++ Lista4/Ex22.java
@@ -7,13 +7,36 @@ import java.util.Scanner;
   país B com Y habitantes e uma taxa de natalidade de 2% ao ano, escrever um
   algoritmo em JAva que seja capaz de calcular e no fim imprimir o tempo necessário
   para que a população do país A ultrapasse a população do país B. Considere que X < Y.
+
+  obs: levando em conta que a população X tem uma taxa de natalidade maior e Y uma taxa de natalidade menor em algum
+  momento em ANOS a população de X será maior, pegando a população fazendo o calculo entre população e porcentagem,
+  resultando no número de habitantes anual com acréscimo.
 */
 
 public class Ex22 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
 
+    float x = 0;
+    float y = 0;
+    float porcentagemX = 0.3f;
+    float porcentagemY = 0.2f;
+    float tempo = 0;
+
+    System.out.println("Digite a população de X sendo a com menor população: ");
+    x = reader.nextFloat();
+    
+    System.out.println("Digite a população de Y sendo a com maior população: ");
+    y = reader.nextFloat();
+    
+    while(x<y) {
+      x = x + (x * porcentagemX);
+      y = y + (y * porcentagemY);
+      tempo++;
+    }
+
+    System.out.printf("Em %.0f anos o país X ira pasar o pais Y", tempo);
+
     reader.close();
   }
-
 }
