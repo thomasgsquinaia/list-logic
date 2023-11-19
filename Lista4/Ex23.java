2859b982bf70989506f3dae174bfcbd97278e330
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-19 19:09:33 -0300

    [main] main: lista04 Ex23 no processo

--- Lista4/Ex23.java
+++ Lista4/Ex23.java
@@ -13,11 +13,23 @@ public class Ex23 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
 
-    float grama = 0;
+    float percaPorcentagem = 0.25f;
+    float massa = 0;
+    float tempo = 0;
+    float hora, min, seg = 0;
 
-    while(grama > 0.10) {
-      
+ 
+    while(massa < 0.10) {
+      System.out.println("Digite o peso em gramas do elemento radioativo: ");
+      massa = reader.nextFloat();
+      massa = massa / percaPorcentagem;
+      tempo=tempo+30;
     }
+    hora = tempo/3600;
+    min = (tempo*60)/60;
+    seg = (tempo/3600)*60;
+
+    System.out.printf("Hora: %.0f Minuto: %.0f Segundo: %.0f", hora, min, seg);
 
     reader.close();
   }
