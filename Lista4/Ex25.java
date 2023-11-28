736eef3f144c6fc13af4975c12e14be6ab12c014
thomasgsquinaia <thomasquinaia@gmail.com>
2023-11-28 08:32:30 -0300

    [main] main: lista04 Ex25 init

---
+++ Lista4/Ex25.java
@@ -0,0 +1,20 @@
+package Lista4;
+
+import java.util.Scanner;
+
+/*
+  Escreva um algoritmo em Java que receba vários números e verifique se eles são ou
+  não quadrados perfeitos. O algoritmo termina a execução quando for digitado um
+  número menor ou igual a 0. 
+  (Um número é quadrado perfeito quando tem umnúmero inteiro como raiz quadrada.). 
+  Obs: não deve ser utilizado métodos da classe Math.
+*/
+
+public class Ex25 {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+
+    
+    reader.close();
+  }
+}
