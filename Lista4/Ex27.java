cf4569ffe47edf3c120872bf2547ee426be4d6ba
thomasgsquinaia <thomasquinaia@gmail.com>
2023-12-13 23:01:50 -0300

    [main] main: ex27 bora uai

--- Lista4/Ex27.java
+++ Lista4/Ex27.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
   Faça um programa que receba um valor inteiro, e imprima na tela o dobro dele, ao
   lado da soma com seu antecessor. Faça isso consecutivamente, imprimindo sempre
   o dobro do valor que apareceu na tela e a soma com o antecessor, por N vezes. O
-  valor de N é fornecido pelo usuário. Por exemplo: Recebe o valor inteiro 2, e N = 3,
+  valor de N é fornecido pelo usuário. Por exemplo: 
+  Recebe o valor inteiro 2, e N = 3,
   então imprime:
-  Dobro = 32
-  Dobro = 4
-  Soma com Antecessor = 4
-  Dobro = 8
-  Soma com Antecessor = 12
-  Dobro = 16
-  Soma com Antecessor = 24
-  Soma com Antecessor = 48
+      0 2 Dobro = 4 Soma com Antecessor = 4       = 4+0
+      1 4 Dobro = 8 Soma com Antecessor = 12      = 8+4
+      2 8 Dobro = 16 Soma com Antecessor = 24     = 16+8
+      3 16 Dobro = 32 Soma com Antecessor = 48    = 32+16
+
   O limite de execução é quando atingir o valor de N, ou quando o dobro ou a soma
   atingirem o valor inteiro de 100.000.
 */
@@ -24,7 +22,11 @@ public class Ex27 {
   public static void main(String[] args) {
     Scanner reader = new Scanner(System.in);
 
-    
+    int n = 0;
+    int fim_da_linha = 100000;
+    while(){}
+    System.out.println("Digite um número: ");
+    n=reader.nextInt();
     reader.close();
   }
 }
