dff37c3566bf160ecb8b54b5dcded87fdd5c4a6c
thomasgsquinaia <thomasquinaia@gmail.com>
2024-04-26 18:20:16 -0300

    [main] - feat: ex02 fix

--- Lista6/Ex02.java
+++ Lista6/Ex02.java
@@ -18,15 +18,30 @@ import java.util.Scanner;
 public class Ex02 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        int n_positivo = 1;
-       
-        while (n_positivo != 0) {
+        
+        System.out.print("Digite um número positivo: ");
+        int n_positivo = reader.nextInt();
+        String resposta = "S";
+        int pares = 0;
+        while (n_positivo > 0) {
             System.out.print("Digite um número positivo: ");
             n_positivo = reader.nextInt();
             if(n_positivo < 0) {
-                System.out.println("Digite um número positivo");
-                return;
+                break;
+            }
+
+            for(int i = 1; i <= n_positivo; i++){ 
+                if(n_positivo % 2 == 0) {
+                    pares = n_positivo;
+                }
+            }
+
+            System.out.printf("Numeros pares entre 1 e %d: %d\n", n_positivo,pares);
+
+
+            if(n_positivo > 0) {
+                System.out.println("Voce deseja prosseguir?");
+                resposta = reader.next();
             }
         }
 
