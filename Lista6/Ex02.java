94cfe161e5c52c602dfc0203e2f97c40b9589f72
thomasgsquinaia <thomasquinaia@gmail.com>
2024-04-26 12:13:34 -0300

    [main] - feat: ex02

---
+++ Lista6/Ex02.java
@@ -0,0 +1,18 @@
+package Lista6;
+
+/*
+    Solicitar um número inteiro positivo ao usuário, validando a entrada de dados
+    (informando se ele estiver errado e repetindo a solicitação até que esteja correto).
+    Após o programa deve informar todos os números pares existentes entre 1 e o
+    número fornecido pelo usuário. ao final ele pergunte ao usuário se ele deseja
+    informar um outro número. Caso positivo, o programa deve ser repetido.
+    Exemplo:
+        Digite um número inteiro positivo: -8 Valor incorreto!
+        Digite um número inteiro positivo: 8 Numero digitado: 8
+        Números inteiros pares entre 1 e 8: 2, 4, 6
+        Você deseja repetir? Não
+ */
+
+public class Ex02 {
+    
+}
