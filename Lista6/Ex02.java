50392472379d30ea8691f1ee8e784eb9ed0b49fc
thomasgsquinaia <thomasquinaia@gmail.com>
2024-04-29 18:48:57 -0300

    [main] - feat: ex02 ok

--- Lista6/Ex02.java
+++ Lista6/Ex02.java
@@ -18,42 +18,54 @@ import java.util.Scanner;
 public class Ex02 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         System.out.print("Digite um número positivo: ");
         int n_positivo = reader.nextInt();
-        String resposta = "S";
+        String opcao;
         int pares = 0;
+
         while (n_positivo > 0) {
             System.out.print("Digite um número positivo: ");
             n_positivo = reader.nextInt();
 
-            if(n_positivo <= 0) {
+            if (n_positivo <= 0) {
                 System.out.println(" Entrou aqui 1 ");
-                while(n_positivo <= 0) {
+                while (n_positivo <= 0) {
                     System.out.print("Digite um número positivo: ");
                     n_positivo = reader.nextInt();
                 }
-            } 
-            else { 
+            } else {
                 System.out.println(" Entrou aqui 2 ");
-                for(int i = 1; i <= n_positivo; i++){ 
-                    if(i % 2 == 0) {
-                        // System.out.printf("é par: %d\n", i);
+                for (int i = 1; i <= n_positivo; i++) {
+                    if (i % 2 == 0) {
                         pares = i;
-                        System.out.printf("Números inteiros pares entre 1 e %d: %d\n", n_positivo,pares);
+                        System.out.printf("Números inteiros pares entre 1 e %d: %d\n", n_positivo, pares);
                     }
-                    // System.out.printf("i: %d\n", i);
-                    // System.out.printf("pares: %d\n", pares);
                 }
 
-                // if(n_positivo > 0) {
-                //     System.out.println("Voce deseja prosseguir?");
-                //     resposta = reader.next();
-                // }
-            }
-          
+                if (n_positivo > 0) {
+                    System.out.println("Você deseja repetir [S/N] ?");
+                    opcao = reader.next();
 
-           
+                    if (opcao.equals("S")) {
+                        System.out.print("Digite um número positivo: ");
+                        n_positivo = reader.nextInt();
+
+                        if (n_positivo <= 0) {
+                            System.out.println(" Entrou aqui 1 ");
+                            while (n_positivo <= 0) {
+                                System.out.print("Digite um número positivo: ");
+                                n_positivo = reader.nextInt();
+                            }
+                        }
+                    } else {
+                        System.out.println("Sistema desligando...");
+                        break;
+                    }
+                }
+
+            }
+
         }
 
         reader.close();
