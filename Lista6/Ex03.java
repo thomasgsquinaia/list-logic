8cd0c0ff410d7d66ff5e1a375c0bb663739fc3ed
thomasgsquinaia <thomasquinaia@gmail.com>
2024-04-29 19:05:30 -0300

    [main] - feat: ex03 init

---
+++ Lista6/Ex03.java
@@ -0,0 +1,15 @@
+package Lista6;
+
+/*
+    Faça um programa que receba a altura e o peso de várias pessoas até que sejam
+    informados os valores zero para a altura. Calcule o IMC e mostre ao final quantas
+    pessoas estão em cada faixa de IMC conforme o quadro a seguir:
+    IMC = Peso / (Altura x Altura)
+    Abaixo do peso - Abaixo de 18,5
+    Peso normal - 18,5 - 24,9
+
+ */
+
+public class Ex03 {
+    
+}
