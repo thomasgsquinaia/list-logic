ce592e7db5b47a071a3da77f7e52e8f47fec8984
thomasgsquinaia <thomasquinaia@gmail.com>
2024-04-29 19:13:04 -0300

    [main] - feat: ex03 run

--- Lista6/Ex03.java
+++ Lista6/Ex03.java
@@ -1,15 +1,38 @@
 package Lista6;
 
+import java.util.Scanner;
+
 /*
     Faça um programa que receba a altura e o peso de várias pessoas até que sejam
     informados os valores zero para a altura. Calcule o IMC e mostre ao final quantas
     pessoas estão em cada faixa de IMC conforme o quadro a seguir:
     IMC = Peso / (Altura x Altura)
+    ============ Tabela =============
     Abaixo do peso - Abaixo de 18,5
     Peso normal - 18,5 - 24,9
-
+    Sobrepeso - 25,0 - 29,9
+    Obesidade Grau I - 30,0 - 34,9
+    Obesidade Grau II - 35,0 - 39,9
+    Obesidade Grau III - 40,0 e acima
  */
 
 public class Ex03 {
-    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        float peso = 0;
+        float altura = 0;
+        
+
+        while(altura != 0) {
+            System.out.println("Digite o peso: ");
+            peso = reader.nextFloat();
+
+            System.out.println("Digite a altura: ");
+            altura = reader.nextFloat();
+        }
+
+
+        reader.close();
+    }
 }
