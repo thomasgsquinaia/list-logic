6259750b65ad3044bf136030de69e21e255ad7bf
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-01 00:27:59 -0300

    [main] - feat: ex04 init

---
+++ Lista6/Ex04.java
@@ -0,0 +1,16 @@
+package Lista6;
+
+import java.util.Scanner;
+
+/*
+    Escrever um algoritmo que calcule os sucessivos valores de E usando a série abaixo
+    e considerando que ovalor de n deve ser fornecido pelo usuário.
+ */
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        reader.close();
+    }    
+}
