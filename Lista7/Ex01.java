58e13a568352f344269a6d9ddc828c6dab25f1a0
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-03 10:52:22 -0300

    [main] - feat: ex01 run

--- Lista7/Ex01.java
+++ Lista7/Ex01.java
@@ -1,5 +1,24 @@
 package Lista7;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
+/*
+    Crie um programa que exibe se um dia é dia útil, fim de semana ou dia inválido dado
+    o número referente ao dia. Considere que domingo é o dia 1 e sábado é o dia 7 
+*/
+
 public class Ex01 {
-    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Calendar calendar = Calendar.getInstance();
+
+        System.out.println("Digite um número para descobrir o dia da semana: ");
+        int diaSemana = reader.nextInt();
+
+        
+
+
+        reader.close();
+    }
 }
