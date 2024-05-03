1a69263bdd6ac8913b7f3efe837ff22c944649ab
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-03 15:20:38 -0300

    [main] - feat: ex01 run

--- Lista7/Ex01.java
+++ Lista7/Ex01.java
@@ -19,7 +19,7 @@ public class Ex01 {
         int dSemana = calendar.get(Calendar.DAY_OF_WEEK);
 
         while (diaSemana != 0) {
-            System.out.println("Digite um número para descobrir o dia da semana: ");
+            System.out.print("Digite um número para descobrir o dia da semana: ");
             dSemana = reader.nextInt();
 
             if(dSemana == 0) { 
@@ -28,39 +28,41 @@ public class Ex01 {
             }
 
             if(dSemana == 1) {
+                System.out.println("Fim de semana");
                 System.out.println("Domingo");
             }
 
             if(dSemana == 2) {
+                System.out.println("Dia útil");
                 System.out.println("Segunda-feira");
             }
 
             if(dSemana == 3) {
+                System.out.println("Dia útil");
                 System.out.println("Terça-feira");
             }
 
             if(dSemana == 4) {
+                System.out.println("Dia útil");
                 System.out.println("Quarta-feira");
             }
 
             if(dSemana == 5) {
+                System.out.println("Dia útil");
                 System.out.println("Quinta-feira");
             }
 
             if(dSemana == 6) {
+                System.out.println("Dia útil");
                 System.out.println("Sexta-Feira");
             }
 
             if(dSemana == 7) {
+                System.out.println("Fim de semana");
                 System.out.println("Sábado");
             }
 
-            
-
-
         }
-
-
 
         reader.close();
     }
