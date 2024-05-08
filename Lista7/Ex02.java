e9d15d6a676086099fb698e5d4cbcd3390ea51a8
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-07 22:32:02 -0300

    [main] => feat: ex02 init

---
+++ Lista7/Ex02.java
@@ -0,0 +1,14 @@
+package Lista7;
+
+/*
+    Uma loja fornece 10% de desconto para funcionários e 5% de desconto para clientes
+    vips. Faça um programa que calcule o valor total a ser pago por uma pessoa. O
+    programa deverá ler o valor total da compra efetuada e um código que identifique se
+    o comprador é um cliente comum (1), funcionário (2) ou vip (3).
+ */
+
+public class Ex02 {
+    public static void main(String[] args) {
+        
+    }
+}
