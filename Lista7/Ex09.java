df3e5a0ed315f9d6d8b9719fd59111d5a40eb28b
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-09 23:06:37 -0300

    [main] => feat: ex09 init

---
+++ Lista7/Ex09.java
@@ -0,0 +1,13 @@
+package Lista7;
+
+/*
+    Criar um programa para identificar se um mês digitado pelo usuário é de alta ou
+    baixa temporada (considerar os seguintes meses como alta temporada: dezembro a
+    fevereiro, junho e julho)
+*/
+
+public class Ex09 {
+    public static void main(String[] args) {
+        
+    }
+}
