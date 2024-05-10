5a73a41280fdd45e244fdd7c848df6d25be9dd4e
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-09 23:11:58 -0300

    [main] => feat: ex09 progress

--- Lista7/Ex09.java
+++ Lista7/Ex09.java
@@ -1,5 +1,7 @@
 package Lista7;
 
+import java.util.Scanner;
+
 /*
     Criar um programa para identificar se um mês digitado pelo usuário é de alta ou
     baixa temporada (considerar os seguintes meses como alta temporada: dezembro a
@@ -8,11 +10,12 @@ package Lista7;
 
 public class Ex09 {
     public static void main(String[] args) {
-        
+        Scanner reader = new Scanner(System.in);
+
         int mes = 0;
 
         System.out.println("Digite o número do mês e saiba se é de baixa ou alta temporada: ");
-        mes = 
+        mes = reader.nextInt();
 
         if(mes == 1 || mes == 2 || mes == 12 || mes == 6 || mes == 7){
             System.out.println("Alta temporada!");
