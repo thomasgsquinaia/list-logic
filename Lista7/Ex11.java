1dba472503f65343d0da913c81a7b0ae703cfe41
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-13 19:10:59 -0300

    [main] => feat: ex11 progress

--- Lista7/Ex11.java
+++ Lista7/Ex11.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Ex11 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        
+
         char soma = '+';
         char subtracao = '-';
         char multiplicacao = 'x';
@@ -26,6 +26,6 @@ public class Ex11 {
 
         
 
-
+        reader.close();
     }
 }
