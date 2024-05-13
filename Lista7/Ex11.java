a262d23f2d6d11a8c0ecca636cc0ed64616ad3cd
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-13 19:04:32 -0300

    [main] => feat: ex11 init

---
+++ Lista7/Ex11.java
@@ -0,0 +1,12 @@
+package Lista7;
+/*
+    Elabore um algoritmo que leia dois valores do usuário e a operação que
+    ele deseja executar como char (Operações: soma, subtração, divisão,
+    multiplicação). Execute a operação desejada e imprima na tela.
+ 
+ */
+public class Ex11 {
+    public static void main(String[] args) {
+        
+    }
+}
