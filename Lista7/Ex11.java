0ab1a2297b4e298bfecf1da899945a04c7191de9
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-14 18:25:12 -0300

    [main] => feat: ex11 progress

--- Lista7/Ex11.java
+++ Lista7/Ex11.java
@@ -1,13 +1,13 @@
 package Lista7;
 
 import java.util.Scanner;
-
 /*
     Elabore um algoritmo que leia dois valores do usuário e a operação que
     ele deseja executar como char (Operações: soma, subtração, divisão,
     multiplicação). Execute a operação desejada e imprima na tela.
  
  */
+
 public class Ex11 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
