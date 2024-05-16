910a307ca58f20a5a984320940e433be449a4a63
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-15 23:38:06 -0300

    [main] => feat: ex01 init

--- Lista8/Ex01.java
+++ Lista8/Ex01.java
@@ -1,6 +1,10 @@
 package Lista8;
 
-public class Ex01 {
+/*
+Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura.
+ */
+
+ public class Ex01 {
     public static void main(String[] args) {
         
     }
