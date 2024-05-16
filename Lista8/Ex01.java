f1c723229efb069ce3c3de3e44dfcba32c7c9cf7
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-15 23:40:22 -0300

    [main] => feat: ex01 init

--- Lista8/Ex01.java
+++ Lista8/Ex01.java
@@ -1,11 +1,15 @@
 package Lista8;
 
+import java.util.Scanner;
+
 /*
 Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura.
  */
 
  public class Ex01 {
     public static void main(String[] args) {
-        
+        Scanner reader = new Scanner(System.in);
+
+        int n = reader.nextInt();
     }
 }
