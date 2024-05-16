b8ed7cb9d1ea280ed4372e65d123847d1c4a0060
thomasgsquinaia <thomasquinaia@gmail.com>
2024-05-15 23:44:11 -0300

    [main] => feat: ex01 progress

--- Lista8/Ex01.java
+++ Lista8/Ex01.java
@@ -13,7 +13,7 @@ Dada uma seqüência de n números, imprimi-la na ordem inversa à da leitura.
         int n = reader.nextInt();
 
         for (int i = 0; i <= n; i++) {
-            System.out.println(i);
+            System.out.printf("%d ",i);
         }
 
         reader.close();
